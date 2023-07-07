607b2949c30039f94b34446626f3f1927deb3cb8
MetalRick84 <lernen@reinitestet.at>
2023-07-07 10:34:29 +0200

    save Note

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-let noteTitle= [];
-let noteText= [];
+let noteTitles= [];
+let noteTexts= [];
 
 function showNewNote() {
     document.getElementById('newNote-bg').classList.remove('display-none');
+}
+
+function hideNewNote() {
+    document.getElementById('newNote-bg').classList.add('display-none');
+}
+
+function storeNotes() {
+    localStorage.setItem('noteTitles', JSON.stringify(noteTitles));
+    localStorage.setItem('noteTexts', JSON.stringify(noteTexts));
 }
 
 function saveNote() {
     let inputTitle= document.getElementById('inputTitle');
     let inputText= document.getElementById('inputText');
 
-    noteTitle.push(inputTitle.value);
-    noteText.push(inputText.value);
+    noteTitles.push(inputTitle.value);
+    noteTexts.push(inputText.value);
+    storeNotes();
+    hideNewNote();
+    inputTitle.value= '';
+    inputText.value= '';
 }
 
