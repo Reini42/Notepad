0fff19a0c813fe696323a643b05e5e02c74acdfa
MetalRick <lernen@reinitestet.at>
2023-07-14 13:55:20 +0200

    quotes

--- js/script-quotes.js
+++ js/script-quotes.js
@@ -1,18 +1,31 @@
 let quoteTexts= [
-    'Es gibt nur einen Weg, um Kritik zu vermeiden: Nichts tun, nichts sagen, nichts sein.',
-    'Mit dem Wissen wächst der Zweifel',
-    'Wahre Worte sind nicht immer schön. Schöne Worte sind nicht immer wahr.',
-    'Tue es oder tue es nicht. Es gibt kein Versuchen.',
-    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+    'Es gibt nur einen Weg, um Kritik zu vermeiden:<br> Nichts tun, nichts sagen, nichts sein.',
+    'Mit dem Wissen <br> wächst der Zweifel',
+    'Wahre Worte sind nicht immer schön.<br> Schöne Worte sind nicht immer wahr.',
+    'Tue es oder tue es nicht.<br> Es gibt kein Versuchen.',
+    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    'Hallo Welt'
 ];
 let quoteAuthors= [
     'Aristoteles',
     'Johann Wolfgang von Goethe',
     'Laozi',
     'Meister Yoda',
-    'Marcus Tullius Cicero'
+    'Marcus Tullius Cicero',
+    'jeder Programmier-Neuling'
 ];
 
 function renderQuotes() {
+    let content= document.getElementById('content');
+
+    content.innerHTML = '';
+    for (let i=0; i < quoteTexts.length; i++) {
+        content.innerHTML += `
+            <div class="note note-quote">
+                <p class="quote-text">${quoteTexts[i]}</p>
+                <h3 class="author">${quoteAuthors[i]}</h3>
+            </div>
+        `;
+    }
     navActivate('nav-quotes');
 }
